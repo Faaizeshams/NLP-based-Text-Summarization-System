f01ee2040180d2269833728d0809529f287429c7
Mohd Faaize Shams <142923967+Faaizeshams@users.noreply.github.com>
2024-07-14 07:27:50 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,12 +7,10 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 import streamlit as st
 import nltk
 
-
-nltk.download('punkt')
-nltk.download('stopwords')
-
+# importing all english letter stopwords int stopword
 stopWords = set(stopwords.words("english"))
 
+# setting up Streamlit interface
 st.title("Text - Summarizer")
 input_text = st.text_area("Enter your text to Summarize: ", height=200)
 col1, col2, col3 = st.columns(3)
@@ -27,14 +25,20 @@ with col1:
 
 text = input_text
 
+# extractive summarization part
 if st.button("Summarize"):
     if option == "Extractive":
+
+        # split the text into words
         words = word_tokenize(text)
 
+        # lower case words of the text
         for i in range(len(words)):
             words[i] = words[i].lower()
 
         freqTable = dict()
+
+        # get  the frequency of all the words other than the stopwords
         for word in words:
             if word not in stopWords:
                 if word not in punctuation:
@@ -43,9 +47,12 @@ if st.button("Summarize"):
                     else:
                         freqTable[word] = 1
 
+        # split the text into sentences
         sentences = sent_tokenize(text)
+
         sentenceValue = dict()
 
+        # then we will get the frequency of all the sentences based on the frequency of words
         for sentence in sentences:
             for word, freq in freqTable.items():
                 if word in sentence.lower():
@@ -54,13 +61,14 @@ if st.button("Summarize"):
                     else:
                         sentenceValue[sentence] = freq
 
-        if sentenceValue:
-            maxValue = max(sentenceValue.values())
+        if sentenceValue:  # Check if sentenceValue is not empty
+            maxValue = max(sentenceValue.values())  # to get the max value
             for sentence in sentenceValue:
-                sentenceValue[sentence] = sentenceValue[sentence] / maxValue
+                sentenceValue[sentence] = sentenceValue[sentence] / maxValue   # making sentence score
 
-            length = int(len(sentenceValue) * 0.35)
+            length = int(len(sentenceValue) * 0.35)  # 35 % total text we will get in the summary
             summary = nlargest(length, sentenceValue, key=sentenceValue.get)
+            # getting the largest sentence score based sentences
 
             Summary = ""
             for sentence in summary:
@@ -75,9 +83,11 @@ if st.button("Summarize"):
 
         model_name = 't5-small'
         tokenizer = T5Tokenizer.from_pretrained(model_name)
+        # converts text into a format the model can understand.
         model = T5ForConditionalGeneration.from_pretrained(model_name)
-
+        # capable of generating text based on input prompts.
         inputs = tokenizer.encode("summarize: " + text, return_tensors="pt")
+        # it instructs the model to perform summarization
         summary_ids = model.generate(inputs, max_length=mlength, min_length=40, length_penalty=1.0, num_beams=10, early_stopping=True)
         Summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
